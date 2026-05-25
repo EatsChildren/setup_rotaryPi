f9d3df8dcedaefddc99d20f1521390d43ca4517c
EatsPets <sstewart.mines.edu>
2026-05-25 06:56:50 -0600

    creating wrapper for rotary phone/bcm functions

---
+++ include/rotary_phone.h
@@ -0,0 +1,59 @@
+#ifndef ROTARY_PHONE_H
+#define ROTARY_PHONE_H
+
+#include <iostream>
+#include <memory>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <thread>
+#include <atomic>
+#include <chrono>
+#include <vector>
+#include <cstdint>
+#include <string>
+#include <mutex>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "bcm2835.h"
+
+#define LED RPI_GPIO_P1_12
+#define OUT RPI_GPIO_P1_11     // 3.3 volts out (to hot header)
+#define HOOK RPI_GPIO_P1_07    // hook -> pin 7
+#define FLAG RPI_GPIO_P1_15    // flag (to tell if someone is about to dial) -> pin 15
+#define COUNTER RPI_GPIO_P1_16 // counts the pulses -> pin 16
+// pin 6 or 9 -> ground (to ground header )
+
+class Rotary_Phone
+{
+
+public:
+    typedef std::shared_ptr<Rotary_Phone> Ptr;
+    static Ptr create() { return std::make_shared<Rotary_Phone>(); }
+    Rotary_Phone();
+    ~Rotary_Phone();
+    void startRX();
+    void closeRX();
+    uint8_t getHook();
+    uint8_t getDialingFlag();
+    uint8_t countPulses();
+    std::vector<uint8_t> getDigits();
+    std::string getPhoneNumber(std::vector<uint8_t> counts);
+    void initializeGPIO();
+
+private:
+    bool _dialing_error;  //if true will play err message via mp3 player until hook goes high
+    std::atomic<bool> _running;
+    uint8_t _hook;
+    uint8_t _flag;
+    std::thread _rx_thread;
+    std::recursive_mutex _mutex;
+    int64_t _ms_delay;
+    unsigned int _bcm_counter_delay;
+
+    void rx_loop();
+};
+
+#endif
---
+++ src/rotary_phone.cpp
@@ -0,0 +1,149 @@
+#include "rotary_phone.h"
+
+Rotary_Phone::Rotary_Phone()
+{
+    _dialing_error = false;
+    _hook = 1;
+    _flag = 0;
+    _running = false;
+    _ms_delay = 10;
+    _bcm_counter_delay = 10;
+    this->initializeGPIO();
+}
+
+Rotary_Phone::~Rotary_Phone()
+{
+    this->closeRX();
+}
+
+void Rotary_Phone::initializeGPIO()
+{
+    try
+    {
+        if (!bcm2835_init())
+        {
+            throw "bcm2835 not initialized";
+        }
+    }
+    catch (const char *msg)
+    {
+        std::cout << "Exception Caught: " << msg;
+    }
+    // initializes the bcm2835 library
+    bcm2835_gpio_fsel(LED, BCM2835_GPIO_FSEL_OUTP);       // sets LET to output
+    bcm2835_gpio_fsel(OUT, BCM2835_GPIO_FSEL_OUTP);       // sets OUT to output
+    bcm2835_gpio_fsel(HOOK, BCM2835_GPIO_FSEL_INPT);      // sets HOOK to input
+    bcm2835_gpio_fsel(FLAG, BCM2835_GPIO_FSEL_INPT);      // sets HOOK to input
+    bcm2835_gpio_set_pud(HOOK, BCM2835_GPIO_PUD_UP);      // Sets the Pull-up mode for the pin.
+    bcm2835_gpio_set_pud(FLAG, BCM2835_GPIO_PUD_DOWN);    // Sets the Pull-up mode for the pin.
+    bcm2835_gpio_fsel(COUNTER, BCM2835_GPIO_FSEL_INPT);   // sets HOOK to input
+    bcm2835_gpio_set_pud(COUNTER, BCM2835_GPIO_PUD_DOWN); // Sets the Pull-up mode for the pin.
+    bcm2835_gpio_set(OUT);                                // turn on the power
+}
+
+void Rotary_Phone::closeRX()
+{
+    _running = false;
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    if (_rx_thread.joinable())
+    {
+        _rx_thread.join();
+    }
+}
+
+void Rotary_Phone::startRX()
+{
+    _running = true;
+    _rx_thread = std::thread(&Rotary_Phone::rx_loop, this);
+}
+
+uint8_t Rotary_Phone::getHook()
+{
+    std::unique_lock<std::recursive_mutex> lock(_mutex);
+    return _hook;
+}
+
+uint8_t Rotary_Phone::getDialingFlag()
+{
+    std::unique_lock<std::recursive_mutex> lock(_mutex);
+    return _flag;
+}
+
+std::vector<uint8_t> Rotary_Phone::getDigits()
+{
+    int num_count = 0;
+    std::vector<uint8_t> nums;
+    while (num_count < 3)
+    {
+        if (!_flag)
+        {
+            nums.push_back(this->countPulses());
+            num_count++;
+        }
+    }
+}
+
+uint8_t Rotary_Phone::countPulses()
+{
+    uint8_t count = 0;
+    uint8_t last = bcm2835_gpio_lev(COUNTER);
+    uint8_t z0 = bcm2835_gpio_lev(COUNTER);
+    uint8_t exitCond = 0;
+    uint8_t exitCond2 = 0;
+    struct timeval start, stop;
+    double secs;
+
+    while (1)
+    {
+
+        last = z0;
+        z0 = bcm2835_gpio_lev(COUNTER);
+        if (z0 == LOW && last == HIGH)
+        {
+            count++;
+            exitCond = 1;
+        }
+
+        if (last == HIGH && z0 == HIGH && exitCond) // signal is staying high
+        {
+            gettimeofday(&start, NULL);
+            exitCond = 0;
+            exitCond2 = 1;
+        }
+
+        gettimeofday(&stop, NULL);
+        if (!exitCond && exitCond2)
+        {
+            secs = (double)(stop.tv_usec - start.tv_usec) * 1e-6 + (double)(stop.tv_sec - start.tv_sec);
+            if (secs > 0.3)
+            {
+                // if the signal is high for more than 300ms then it is done counting and can exit the function
+                break;
+            }
+        }
+        bcm2835_delay(_bcm_counter_delay); // should delay for 10 ms
+    }
+    if (count > 9)
+    {
+        count = 0;
+    }
+    return count;
+}
+
+// ######################################################
+// ######################## RX LOOP #####################
+// ######################################################
+
+void Rotary_Phone::rx_loop()
+{
+
+    while (_running)
+    {
+        {
+            std::unique_lock<std::recursive_mutex> lock(_mutex);
+            _hook = bcm2835_gpio_lev(HOOK);
+            _flag = bcm2835_gpio_lev(FLAG);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(_ms_delay));
+    }
+}
